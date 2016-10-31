e5f65cb0f2c3b04cdde7f5b3e102f40a455697da
Marc Görtz <mail@marcgoertz.de>
2016-10-31 18:02:29 +0100

    Restructure files.

--- htdocs/index.php
+++ htdocs/index.php
@@ -22,7 +22,7 @@ if (
 // @TODO: check for valid IATA codes
 
 // read flightdata
-$flightData = array_map('str_getcsv', file(dirname(__FILE__) . '/vendor/openflights/airports.dat'));
+$flightData = array_map('str_getcsv', file(dirname(__FILE__) . '/../vendor/openflights/airports.dat'));
 $tzData = array();
 foreach ($flightData as $data) {
 	if ($data[11] !== '\N') {
@@ -42,6 +42,3 @@ $diff = $toLocal->diff($fromLocal, true);
 $duration = $diff->format("P%dDT%hH%iM");
 
 echo '{"from":"' . $fromLocal->format("Y-m-d\TH:iP") . '","to":"' . $toLocal->format("Y-m-d\TH:iP") . '","duration":"' . $duration . '"}';
-
-// http --json --body http://localhost from==2016-12-14T13:50 to==2016-12-15T06:50 departureAirport==ZRH destinationAirport==HKT
-// http --json --body http://localhost from==2016-11-10T16:25 to==2016-11-10T20:40 departureAirport==AMS destinationAirport==MIA
